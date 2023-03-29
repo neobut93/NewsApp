f5b933558d717e7ab9ed4a7fbf9a8afe67d66ce1
Guest2 <guest2@Ashleys-MacBook-Air.local>
2023-03-29 07:55:47 -0700

    added nav controller

---
+++ app/src/main/java/com/example/newsapp/ui/screen/DetailScreen.kt
@@ -0,0 +1,35 @@
+package com.example.newsapp.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DetailScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Detail Screen", fontWeight = FontWeight.SemiBold)
+        Button(onClick = {
+            //navController.navigate("TopNews")
+            navController.popBackStack()
+        }) {
+            Text(text = "Go to Top News Screen")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    DetailScreen(rememberNavController())
+}
